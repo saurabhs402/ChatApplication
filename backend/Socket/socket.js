e0e0f61ae135369e65ee1c323178edcebefa4193
Saurabh <codefun402@gmail.com>
2024-05-13 04:17:31 +0530

    Socket link update

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     //frontend url
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["https://convo-craft-six.vercel.app/"],
         methods: ["GET", "POST"],
         
     },
